c764f60430ec203e67b1e609965fd581c8bab949
Bhuyash <bhuyash.pvt@gmail.com>
2024-05-22 22:42:01 +0530

    Script added

---
+++ BankingSystem.sql
@@ -0,0 +1,17 @@
+USE BankingSystemDb;
+SHOW TABLES;
+SELECT * FROM Account
+
+
+
+CREATE TABLE Account (
+    AccountId INT PRIMARY KEY,
+    AccountName VARCHAR(50),
+    Balance DOUBLE
+);
+
+
+INSERT INTO Account (AccountId, AccountName, Balance) VALUES 
+
+
+
